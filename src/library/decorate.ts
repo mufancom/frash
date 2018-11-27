b7619f83615db958939c13e76fda475f2e12df5d
Dizy Zeng <mhisf@vip.qq.com>
2018-11-27 19:21:55 +0800

    Project structure

---
+++ src/library/decorate.ts
@@ -0,0 +1,43 @@
+export function decorate<T>(
+  constructor: new (...args: any[]) => T,
+  decorators: {
+    [P in keyof T]?:
+      | MethodDecorator
+      | PropertyDecorator
+      | Array<MethodDecorator>
+      | Array<PropertyDecorator>
+  },
+): void;
+export function decorate<T>(
+  object: T,
+  decorators: {
+    [P in keyof T]?:
+      | MethodDecorator
+      | PropertyDecorator
+      | Array<MethodDecorator>
+      | Array<PropertyDecorator>
+  },
+): T;
+
+export function decorate(thing: any, decorators: any) {
+  let target = typeof thing === 'function' ? thing.prototype : thing;
+
+  for (let prop in decorators) {
+    let propertyDecorators = decorators[prop];
+
+    if (!Array.isArray(propertyDecorators)) {
+      propertyDecorators = [propertyDecorators];
+    }
+
+    let descriptor = Object.getOwnPropertyDescriptor(target, prop);
+    let newDescriptor = propertyDecorators.reduce(
+      (accDescriptor: any, decorator: any) =>
+        decorator(target, prop, accDescriptor),
+      descriptor,
+    );
+
+    if (newDescriptor) Object.defineProperty(target, prop, newDescriptor);
+  }
+
+  return thing;
+}
