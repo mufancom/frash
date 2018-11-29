b2dfb1b545c7dde7afdfb6bd313210b5b99e820e
Dizy Zeng <mhisf@vip.qq.com>
2018-11-30 02:13:10 +0800

    Fix lint

--- src/library/utils/iterable.ts
+++ src/library/utils/iterable.ts
@@ -1,7 +1,7 @@
 export function makeIterable<T>(_iterator: Iterator<T>): IterableIterator<T> {
   let iterator = _iterator as any;
   iterator[Symbol.iterator] = self;
-  return iterator as any;
+  return iterator;
 }
 
 function self<T>(this: T): T {
